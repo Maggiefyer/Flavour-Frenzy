928b56e811e78eb56287b759c48d1cefed4a9d0f
Maggiefyer <109766140@qq.com>
2023-03-27 14:35:46 +1300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Flavour_Frenzy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
